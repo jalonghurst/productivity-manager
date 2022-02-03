900ea9cd4c12fc866d52874df737b69fed765a4a
unknown <jacqui.longhurst@btinternet.com>
2022-02-03 18:33:53 +0100

    Edited Header/Navigtion bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
-};
+  return (
+  <AppBar position='static'>
+      <Container>
+          <Toolbar>
+              <Typography variant='h6'>
+                  Productivity Manager
+              </Typography>
+          </Toolbar>
+      </Container>
+</AppBar>
+  )};
 
 export default Header;

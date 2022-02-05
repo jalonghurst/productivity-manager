96bbf1de3beeae0f21af252135779cf39d030d0e
unknown <jacqui.longhurst@btinternet.com>
2022-02-05 12:35:18 +0100

    update pages

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Pomodorotimer from './Pomodorotimer';
+import Taskoverview from './Taskoverview'
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+  <>
+  <Pomodorotimer/>
+  <Taskoverview/>
+  </>
+  )
 };
 
 export default Homepage;

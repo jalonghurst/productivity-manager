7ab6d57dfd40440555fc5db27a201c017d08e6c5
unknown <jacqui.longhurst@btinternet.com>
2022-02-05 14:18:47 +0100

    Banner creation in Homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../components/Banner/Banner';
 import Pomodorotimer from './Pomodorotimer';
 import Taskoverview from './Taskoverview'
 
 const Homepage = () => {
   return (
   <>
+  <Banner/>
   <Pomodorotimer/>
   <Taskoverview/>
   </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,64 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
-import React from 'react';
+import {
+    AppBar,
+    Container,
+    Toolbar,
+    Typography,
+    Button,
+    MenuItem,
+  } from "@material-ui/core";
+  import {
+    createTheme,
+    makeStyles,
+    ThemeProvider,
+  } from "@material-ui/core/styles";
+  import { useHistory } from "react-router-dom";
 
-const Header = () => {
-  return (
-  <AppBar position='static'>
-      <Container>
-          <Toolbar>
-              <Typography variant='h6'>
-                  Productivity Manager
+  
+  const useStyles = makeStyles((theme) => ({
+    title: {
+      flex: 1,
+      color: "gold",
+      fontFamily: "Montserrat",
+      fontWeight: "bold",
+      cursor: "pointer",
+    },
+  }));
+  
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  
+  function Header() {
+    const classes = useStyles();
+   
+  
+    const history = useHistory();
+  
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <AppBar color="transparent" position="static">
+          <Container>
+            <Toolbar>
+              <Typography
+                onClick={() => history.push(`/`)}
+                variant="h6"
+                className={classes.title}
+              >
+                Productivity Pro
               </Typography>
-          </Toolbar>
-      </Container>
-</AppBar>
-  )};
-
-export default Header;
+              <MenuItem> Dashboard</MenuItem>
+              <Button color="inherit">Login</Button>
+          
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </ThemeProvider>
+    );
+  }
+  
+  export default Header;
